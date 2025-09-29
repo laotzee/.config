9582c5db2bd2cdc99fbabe4d3d2035fcce888f93
laotzee <laotzeabreu@proton.me>
2025-09-29 04:56:29 -0400

    Create script for updating config file repo if there are changes

---
+++ scripts/updateConfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rsync ~/.config/nvim/init.lua ~/projects/.config/nvim/init.lua
+
+rsync ~/.config/i3/config ~/projects/.config/i3/config
+rsync ~/.config/i3/vol.sh ~/projects/.config/i3/vol.sh
+
+rsync ~/.bashrc ~/projects/.config/.bashrc
