68e3c963d8913d3eae3656fedc51b5c6a04b1c66
laotzee <laotzeabreu@proton.me>
2025-10-03 16:05:57 -0400

    Move .bashrc to scripts folder

--- scripts/updateConfig.sh
+++ scripts/updateConfig.sh
@@ -5,4 +5,4 @@ rsync ~/.config/nvim/init.lua ~/projects/.config/nvim/init.lua
 rsync ~/.config/i3/config ~/projects/.config/i3/config
 rsync ~/.config/i3/vol.sh ~/projects/.config/i3/vol.sh
 
-rsync ~/.bashrc ~/projects/.config/.bashrc
+rsync ~/.bashrc ~/projects/.config/scripts/.bashrc
