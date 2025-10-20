38e8badb9ca834349cf0f60a567d794616b8de88
laotzee <laotzeabreu@proton.me>
2025-10-20 14:08:51 -0400

    Add new config files to track

--- scripts/updateConfig.sh
+++ scripts/updateConfig.sh
@@ -4,5 +4,13 @@ rsync ~/.config/nvim/init.lua ~/projects/.config/nvim/init.lua
 
 rsync ~/.config/i3/config ~/projects/.config/i3/config
 rsync ~/.config/i3/vol.sh ~/projects/.config/i3/vol.sh
+rsync ~/.config/i3status/config ~/projects/.config/i3status/config
 
 rsync ~/.bashrc ~/projects/.config/scripts/.bashrc
+
+rsync ~/.bashrc ~/projects/.config/scripts/.bashrc
+
+rsync ~/.config/picom/picom.conf ~/projects/.config/picom/picom.conf
+
+rsync ~/.config/lynx/lynx.cfg ~/projects/.config/lynx/lynx.cfg
+rsync ~/.config/lynx/lynx.lss ~/projects/.config/lynx/lynx.lss
