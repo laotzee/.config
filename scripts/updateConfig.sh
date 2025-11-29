7d98cf55ba1a70d37da9fc0134ec8fc9095611b4
laotzee <laotzeabreu@proton.me>
2025-11-29 11:59:29 -0500

    Improve config look up

--- scripts/updateConfig.sh
+++ scripts/updateConfig.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+#nvim
 rsync ~/.config/nvim/init.lua ~/projects/.config/nvim/init.lua
 
+#i3
 rsync ~/.config/i3/config ~/projects/.config/i3/config
 rsync ~/.config/i3/vol.sh ~/projects/.config/i3/vol.sh
 rsync ~/.config/i3status/config ~/projects/.config/i3status/config
 
+#bashrc
 rsync ~/.bashrc ~/projects/.config/scripts/.bashrc
 
-rsync ~/.bashrc ~/projects/.config/scripts/.bashrc
-
+#picom
 rsync ~/.config/picom/picom.conf ~/projects/.config/picom/picom.conf
 
+#lynx
 rsync ~/.config/lynx/lynx.cfg ~/projects/.config/lynx/lynx.cfg
 rsync ~/.config/lynx/lynx.lss ~/projects/.config/lynx/lynx.lss
